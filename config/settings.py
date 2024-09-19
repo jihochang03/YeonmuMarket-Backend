dd37eac2a17f0238385b044a52b93709476dd10e
hoyah <hoyaho03@snu.ac.kr>
2024-09-19 10:36:14 +0900

    초기설정

--- config/settings.py
+++ config/settings.py
@@ -102,9 +102,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yeonmumarketdb',          # The name of your PostgreSQL database
+        'USER': 'admin',           # Your PostgreSQL username
+        'PASSWORD': '2024mutsha',      # Your PostgreSQL password
+        'HOST': 'localhost',              # Or use an IP if your DB is hosted elsewhere
+        'PORT': '5432',                   # Default PostgreSQL port
     }
+
 }
 
 
