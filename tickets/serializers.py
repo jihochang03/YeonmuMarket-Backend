0db554037ba8965798aa55da4da209d353ef1ba7
Taehyeungkim <kth1837@snu.ac.kr>
2024-10-05 04:02:54 +0900

    chore: renamed ticket views to ticket post views

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Ticket
+from .models import Ticket, TicketPost
 
 class TicketSerializer(ModelSerializer):
     class Meta:
         model = Ticket
+        fields = "__all__"
+
+class TicketPostSerializer(ModelSerializer):
+    class Meta:
+        model = TicketPost
         fields = "__all__"
