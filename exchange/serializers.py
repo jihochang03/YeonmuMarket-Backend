24698ad0d02f280c423b6dae5ec56daaae167f60
hoyah <hoyaho03@snu.ac.kr>
2025-01-01 09:49:25 +0900

    교환

--- exchange/serializers.py
+++ exchange/serializers.py
@@ -3,14 +3,16 @@ from rest_framework import serializers
 from .models import Exchange
 # TicketSerializer를 ExchangeSerializer 클래스 내에서 임포트
 class ExchangeSerializer(serializers.ModelSerializer):
+    ticket_1 = serializers.SerializerMethodField()
+    ticket_2 = serializers.SerializerMethodField()
     class Meta:
         model = Exchange
         fields = "__all__"
 
     def get_ticket_1(self, obj):
         from tickets.serializers import TicketSerializer
-        return TicketSerializer(obj.ticket_1).data  # TicketSerializer를 내부에서 임포트하여 사용
-    
+        return TicketSerializer(obj.ticket_1, context=self.context).data
+
     def get_ticket_2(self, obj):
         from tickets.serializers import TicketSerializer
-        return TicketSerializer(obj.ticket_2).data  # TicketSerializer를 내부에서 임포트하여 사용
+        return TicketSerializer(obj.ticket_2, context=self.context).data
